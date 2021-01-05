f859eb31ea8beb81caeb717dec534e0eb0398f22
Rohan <60407482+Rohan-yadav@users.noreply.github.com>
2021-01-05 11:49:15 -0500

    Create FindASCII.cpp

---
+++ Basic/FindASCII.cpp
@@ -0,0 +1,17 @@
+//This program is to convert character into its ascii value
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+        char a;
+        int ascii;
+
+        cout<<"enter char to print ascii value";
+        cin>>a;
+        ascii = (int)a;
+        cout<<"ASCII value of "<<a<<"is "<<ascii;
+
+}
